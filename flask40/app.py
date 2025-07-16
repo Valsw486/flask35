1494cc6ea96d972f15e25ba11ce9ec46479b6c8b
Valsw486 <Valsw4@gmail.com>
2025-07-17 02:46:41 +0300

    2025

--- flask40/app.py
+++ flask40/app.py
@@ -3,16 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
-
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
